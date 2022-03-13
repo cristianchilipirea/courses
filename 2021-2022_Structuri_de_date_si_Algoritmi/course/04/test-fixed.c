53fe57a3c791f61e63aa4cd58cc8b430e79fd2b1
cristian.chilipirea <cristian.chilipirea@gmail.com>
2022-03-13 19:25:44 +0200

    SDA curs 04

---
+++ 2021-2022_Structuri_de_date_si_Algoritmi/course/04/test-fixed.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int fun1()
+{
+	return 1;
+}
+
+void fun2()
+{
+	int d = 0;
+	fun1();
+	return;
+}
+int main()
+{
+	int a = 5;
+	int v[4] = {0, 1, 2, 3};
+	int *vv = (int *)malloc(5 * sizeof(int));
+	vv[3] = 5;
+	fun2();
+	a = fun1();
+	return 0;
+}
